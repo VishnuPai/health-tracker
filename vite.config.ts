af5e6fa79d2a8237b44ef2a46f4bdc3556bb02d2
Vishnu Pai <vishnu.pai@volvo.com>
2026-02-07 12:49:08 +0530

    fix: Force PWA cache bust and update service worker logic

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'Health Tracker',
